7875e0856d7fe889003c39a74c0b884c0c55e45d
tortortortortortor <114900680+tortortortortortor@users.noreply.github.com>
2023-11-24 09:58:44 +0100

    Solves, but keep in mind only with four days

--- healthcare_schedule.py
+++ healthcare_schedule.py
@@ -34,9 +34,38 @@ class HealthcareSchedule:
         self._add_isolated_day_constraints()
         self._add_weekend_work_constraints()
         self._add_shift_type_constraints()
-
+        self._add_max_days_worked_constraints()
         # ... other constraints
         self._compile_objective_function()
+
+    # This constraint will try to set the maximum number of days worked in a 7-day period
+    def _add_max_days_worked_constraints(self):
+        max_days_in_7 = 4  # Relaxing the constraint to 4 days in a 7-day period
+
+        for staff_member, info in self.staff_info.items():
+            if info["shift"] == "D1":
+                for week in range(self.num_weeks):
+                    for start_day in range(self.days_per_week):
+                        # Calculate the end day and adjust for week transition
+                        end_day = start_day + 7
+                        if end_day > self.days_per_week:
+                            # Window spans two weeks
+                            days_in_current_week = self.days_per_week - start_day
+                            days_in_next_week = end_day - self.days_per_week
+                            next_week = (week + 1) % self.num_weeks
+
+                            # Sum shifts across the 7-day window spanning two weeks
+                            shift_sum = pulp.lpSum(self.shifts[staff_member, week, day, "D1"] 
+                                                   for day in range(start_day, self.days_per_week)) + \
+                                        pulp.lpSum(self.shifts[staff_member, next_week, day, "D1"] 
+                                                   for day in range(days_in_next_week))
+                        else:
+                            # Window within a single week
+                            shift_sum = pulp.lpSum(self.shifts[staff_member, week, day, "D1"] 
+                                                   for day in range(start_day, end_day))
+
+                        # Apply the constraint
+                        self.problem += (shift_sum <= max_days_in_7, f"Max_{max_days_in_7}_D1_Shifts_{staff_member}_Week{week}_StartDay{start_day}")
 
     def _add_shift_type_constraints(self):
         for week in range(self.num_weeks):
