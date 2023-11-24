7e263835e57f0ff1cc0cd47f5be1c11f21086d68
tortortortortortor <114900680+tortortortortortor@users.noreply.github.com>
2023-11-24 09:08:00 +0100

    Added one constraint

--- healthcare_schedule.py
+++ healthcare_schedule.py
@@ -8,6 +8,7 @@ class HealthcareSchedule:
         self.shift_hours = shift_hours
         self.problem = pulp.LpProblem("Healthcare_Scheduling", pulp.LpMaximize)
         self.shifts = None
+        self.objective_function_components = []  # Initialize the list to store objective function components
         self.initialize_variables()
 
     def initialize_variables(self):
@@ -27,14 +28,49 @@ class HealthcareSchedule:
         self._add_work_hours_constraints()
         self._add_isolated_day_constraints()
         # ... other constraints
+        self._compile_objective_function()
+
 
     def _add_work_hours_constraints(self):
         # Implementation of work hours constraints
         pass
 
     def _add_isolated_day_constraints(self):
-        # Implementation of isolated day constraints
-        pass
+        isolated_day_penalty_weight = 100
+        self.isolated_work_vars = {}
+        self.isolated_off_vars = {}
+
+        for staff_member in self.staff_info:
+            for week in range(self.num_weeks):
+                for day in range(self.days_per_week):
+                    self._add_single_isolated_day_constraint(staff_member, week, day, isolated_day_penalty_weight)
+
+    def _add_single_isolated_day_constraint(self, staff_member, week, day, penalty_weight):
+        # Create variables for isolated work and off days
+        isolated_work_var = pulp.LpVariable(f"isolated_work_{staff_member}_{week}_{day}", cat='Binary')
+        isolated_off_var = pulp.LpVariable(f"isolated_off_{staff_member}_{week}_{day}", cat='Binary')
+        self.isolated_work_vars[(staff_member, week, day)] = isolated_work_var
+        self.isolated_off_vars[(staff_member, week, day)] = isolated_off_var
+
+        # Constraint for isolated working day
+        if day == 0:  # First day of the week
+            self.problem += isolated_work_var >= self.shifts[staff_member, week, day, self.staff_info[staff_member]["shift"]] - (self.shifts[staff_member, week, day + 1, self.staff_info[staff_member]["shift"]] if day + 1 < self.days_per_week else 0)
+        elif day == self.days_per_week - 1:  # Last day of the week
+            self.problem += isolated_work_var >= self.shifts[staff_member, week, day, self.staff_info[staff_member]["shift"]] - self.shifts[staff_member, week, day - 1, self.staff_info[staff_member]["shift"]]
+        else:  # Other days
+            self.problem += isolated_work_var >= self.shifts[staff_member, week, day, self.staff_info[staff_member]["shift"]] - (self.shifts[staff_member, week, day - 1, self.staff_info[staff_member]["shift"]] + self.shifts[staff_member, week, day + 1, self.staff_info[staff_member]["shift"]])
+
+        # Constraint for isolated off day
+        if day == 0:
+            self.problem += isolated_off_var >= (1 - self.shifts[staff_member, week, day, self.staff_info[staff_member]["shift"]]) - (1 - self.shifts[staff_member, week, day + 1, self.staff_info[staff_member]["shift"]] if day + 1 < self.days_per_week else 0)
+        elif day == self.days_per_week - 1:
+            self.problem += isolated_off_var >= (1 - self.shifts[staff_member, week, day, self.staff_info[staff_member]["shift"]]) - (1 - self.shifts[staff_member, week, day - 1, self.staff_info[staff_member]["shift"]])
+        else:
+            self.problem += isolated_off_var >= (1 - self.shifts[staff_member, week, day, self.staff_info[staff_member]["shift"]]) - ((1 - self.shifts[staff_member, week, day - 1, self.staff_info[staff_member]["shift"]]) + (1 - self.shifts[staff_member, week, day + 1, self.staff_info[staff_member]["shift"]]))
+
+        # Add penalty for isolated days to the objective function
+        self.objective_function_components.append(-penalty_weight * (isolated_work_var + isolated_off_var))
+
 
     def set_objective(self):
         # Set the objective function
@@ -47,3 +83,6 @@ class HealthcareSchedule:
     def generate_report(self):
         # Generate a report of the solution
         pass
+    
+    def _compile_objective_function(self):
+        self.problem += pulp.lpSum(self.objective_function_components)
